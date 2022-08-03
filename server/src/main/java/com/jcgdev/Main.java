c51f9e67c910b387513f1f4959469658d31210aa
JCGdev <53711291+JCGdev@users.noreply.github.com>
2022-08-03 16:29:29 +0200

    Removed testing code from Main.java

--- server/src/main/java/com/jcgdev/Main.java
+++ server/src/main/java/com/jcgdev/Main.java
@@ -15,20 +15,7 @@ public class Main {
 	private static SpringApplication apiRest = new SpringApplication(Main.class);
 	
 	public static void main(String[] args) {
-		apiRest.setDefaultProperties(Collections.singletonMap("server.port",
-															  "8083"));
 		apiRest.run(args);
-		
-//		String cc1 = "4915110176928790";
-//		String cc2 = "4915110191768499";
-//		
-//		CreditCard creditCard1 = new CreditCard(cc1, null, null, null);
-//		CreditCard creditCard2 = new CreditCard(cc2, null, null, null);
-//	
-//		CreditCard extrapolated = new SofiaAlgorithm().execute(new CreditCard[] {creditCard1, 
-//																					creditCard2});
-//		System.out.println(extrapolated.getNumber());
-																																						
 		
 	}
 
